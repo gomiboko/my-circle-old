21d1e416eeadcc6237608ba5ef608bf17c0feadc
gomiboko <gomiboko1123@gmail.com>
2021-07-09 23:19:50 +0900

    [add] 開発用DBを用意

---
+++ back-end/db/init/1_ddl.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE mycircle;
+USE mycircle;
+CREATE TABLE IF NOT EXISTS `mycircle`.`users` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  `email` VARCHAR(254) NOT NULL,
+  `password_hash` VARCHAR(72) NOT NULL,
+  `created_at` DATETIME NOT NULL,
+  `updated_at` DATETIME NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE)
+ENGINE = InnoDB;
---
+++ back-end/db/init/2_dml.sql
@@ -0,0 +1,2 @@
+USE mycircle;
+INSERT INTO users (name, email, password_hash, created_at, updated_at) VALUES ("ユーザ1", "foo@example.com", "password", NOW(), NOW());
