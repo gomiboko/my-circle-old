7c762bc35c80eb326e52bc5e9513f397333d30ff
gomiboko <gomiboko1123@gmail.com>
2021-08-13 17:47:05 +0900

    [add] コントローラのテストを追加

---
+++ back-end/controllers/auth_controller_test.go
@@ -0,0 +1,239 @@
+package controllers
+
+import (
+	"encoding/json"
+	"errors"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+	"github.com/gomiboko/my-circle/models"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+const validEmail = "foo@example.com"
+const validPassword = "password123"
+const emailMaxLength = 254
+const passwordMinLength = 8
+const passwordMaxLength = 64
+
+type apiResponse struct {
+	Msg  string
+	User models.User
+}
+
+type userRepositoryMock struct {
+	mock.Mock
+}
+
+func (m *userRepositoryMock) GetUser(email string, password string) (*models.User, error) {
+	args := m.Called(email, password)
+	return args.Get(0).(*models.User), args.Error(1)
+}
+
+func TestLoginValidationNG(t *testing.T) {
+	inputs := []struct {
+		email    string
+		password string
+	}{
+		// メールアドレスのチェックデータ
+		{
+			email:    "",
+			password: validPassword,
+		},
+		{
+			email:    "isNotEmail",
+			password: validPassword,
+		},
+		{
+			email:    createEmailAddress(emailMaxLength + 1),
+			password: validPassword,
+		},
+		// パスワードのチェックデータ
+		{
+			email:    validEmail,
+			password: "",
+		},
+		{
+			email:    validEmail,
+			password: strings.Repeat("a", passwordMinLength-1),
+		},
+		{
+			email:    validEmail,
+			password: strings.Repeat("a", passwordMaxLength+1),
+		},
+		{
+			email:    validEmail,
+			password: "にほんごぱすわーど",
+		},
+	}
+
+	ac := NewAuthController(nil)
+
+	for _, in := range inputs {
+		reqBody := createRequestBody(in.email, in.password)
+		r, c := createLoginPostContext(reqBody)
+
+		ac.Login(c)
+
+		var res apiResponse
+		json.Unmarshal(r.Body.Bytes(), &res)
+
+		assert.Equal(t, http.StatusBadRequest, r.Code)
+		assert.Equal(t, "入力エラー", res.Msg)
+	}
+}
+
+func TestLoginValidationOK(t *testing.T) {
+	inputs := []struct {
+		email    string
+		password string
+	}{
+		// メールアドレスのチェックデータ
+		{
+			email:    createEmailAddress(emailMaxLength),
+			password: validPassword,
+		},
+		{
+			email:    "にほんご@example.com",
+			password: validPassword,
+		},
+		// パスワードのチェックデータ
+		{
+			email:    validEmail,
+			password: strings.Repeat("a", passwordMinLength),
+		},
+		{
+			email:    validEmail,
+			password: strings.Repeat("a", passwordMaxLength),
+		},
+	}
+
+	user := &models.User{
+		Email:        validEmail,
+		PasswordHash: "",
+	}
+
+	urMock := new(userRepositoryMock)
+	urMock.On("GetUser", mock.AnythingOfType("string"), mock.AnythingOfType("string")).Return(user, nil)
+
+	ac := NewAuthController(urMock)
+
+	for _, in := range inputs {
+		reqBody := createRequestBody(in.email, in.password)
+		r, c := createLoginPostContext(reqBody)
+
+		ac.Login(c)
+
+		var res apiResponse
+		json.Unmarshal(r.Body.Bytes(), &res)
+
+		assert.Equal(t, http.StatusOK, r.Code)
+		assert.Equal(t, "logged in", res.Msg)
+	}
+}
+
+func TestSucceedLogin(t *testing.T) {
+	user := &models.User{
+		Email:        validEmail,
+		PasswordHash: "",
+	}
+
+	urMock := new(userRepositoryMock)
+	urMock.On("GetUser", validEmail, validPassword).Return(user, nil)
+
+	ac := NewAuthController(urMock)
+
+	reqBody := createRequestBody(validEmail, validPassword)
+	r, c := createLoginPostContext(reqBody)
+
+	ac.Login(c)
+
+	var res apiResponse
+	json.Unmarshal(r.Body.Bytes(), &res)
+
+	assert.Equal(t, http.StatusOK, r.Code)
+	assert.Equal(t, "logged in", res.Msg)
+	assert.Equal(t, validEmail, res.User.Email)
+	assert.Equal(t, "", res.User.PasswordHash)
+}
+
+func TestFailToLogin(t *testing.T) {
+	var user *models.User
+
+	// 認証NGの場合、ユーザデータの返却値がnil
+	urMock := new(userRepositoryMock)
+	urMock.On("GetUser", validEmail, validPassword).Return(user, nil)
+
+	ac := NewAuthController(urMock)
+
+	reqBody := createRequestBody(validEmail, validPassword)
+	r, c := createLoginPostContext(reqBody)
+
+	ac.Login(c)
+
+	var res apiResponse
+	json.Unmarshal(r.Body.Bytes(), &res)
+
+	assert.Equal(t, http.StatusUnauthorized, r.Code)
+	assert.Equal(t, "認証エラー", res.Msg)
+}
+
+func TestUnexpectedError(t *testing.T) {
+	var user *models.User
+
+	urMock := new(userRepositoryMock)
+	urMock.On("GetUser", validEmail, validPassword).Return(user, errors.New("test exception"))
+
+	ac := NewAuthController(urMock)
+
+	reqBody := createRequestBody(validEmail, validPassword)
+	r, c := createLoginPostContext(reqBody)
+
+	ac.Login(c)
+
+	var res apiResponse
+	json.Unmarshal(r.Body.Bytes(), &res)
+
+	assert.Equal(t, r.Code, http.StatusInternalServerError)
+	assert.Equal(t, "予期せぬエラー", res.Msg)
+}
+
+func TestLogout(t *testing.T) {
+	ac := NewAuthController(nil)
+
+	w := httptest.NewRecorder()
+	c, _ := gin.CreateTestContext(w)
+	c.Request, _ = http.NewRequest(http.MethodGet, "/logout", nil)
+
+	ac.Logout(c)
+
+	var res apiResponse
+	json.Unmarshal(w.Body.Bytes(), &res)
+
+	assert.Equal(t, http.StatusOK, w.Code)
+	assert.Equal(t, "logged out", res.Msg)
+}
+
+// 指定の長さのメールアドレスを生成する
+func createEmailAddress(length int) string {
+	return strings.Repeat("a", length-len("@example.com")) + "@example.com"
+}
+
+// ログインのリクエストボディ文字列を生成する
+func createRequestBody(email string, password string) string {
+	return `{"email":"` + email + `","password":"` + password + `"}`
+}
+
+// ログインリクエストのGinコンテキストを生成する
+func createLoginPostContext(reqBody string) (*httptest.ResponseRecorder, *gin.Context) {
+	r := httptest.NewRecorder()
+	c, _ := gin.CreateTestContext(r)
+	c.Request, _ = http.NewRequest(http.MethodPost, "/login", strings.NewReader(reqBody))
+	c.Request.Header.Set("Content-Type", "application/json")
+
+	return r, c
+}
