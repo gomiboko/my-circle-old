bb0358d9eb2355e82120b541b7d28c256dca79a9
gomiboko <gomiboko1123@gmail.com>
2021-10-18 22:22:34 +0900

    [add] Alert.vueのテスト

---
+++ front-end/tests/unit/Alert.spec.ts
@@ -0,0 +1,59 @@
+import { shallowMount } from "@vue/test-utils";
+import Alert from "@/components/Alert.vue";
+import { MessageType } from "@/utils/message";
+
+describe("Alert.vue", () => {
+  describe("messageプロパティ", () => {
+    const props = {
+      type: MessageType.Info,
+      message: "",
+    };
+
+    describe("空の場合", () => {
+      test("メッセージが表示されないこと", () => {
+        props.message = "";
+        const wrapper = shallowMount(Alert, { propsData: props });
+        expect(wrapper.text()).toBe("");
+      });
+    });
+
+    describe("空でない場合", () => {
+      test("設定されている値が表示されること", () => {
+        props.message = "テストメッセージ";
+        const wrapper = shallowMount(Alert, { propsData: props });
+        expect(wrapper.text()).toBe("テストメッセージ");
+      });
+    });
+
+    describe("HTMLタグの場合", () => {
+      test("エスケープされること", () => {
+        props.message = "<p>tag</p>";
+        const warpper = shallowMount(Alert, { propsData: props });
+        expect(warpper.text()).toBe("<p>tag</p>");
+      });
+    });
+  });
+
+  describe("typeプロパティ", () => {
+    const props = {
+      type: MessageType.Info,
+      message: "test",
+    };
+
+    describe("MessageTypeの各タイプが指定された場合", () => {
+      test("指定されたタイプで表示されること", () => {
+        const testType = (t: MessageType) => {
+          props.type = t;
+          const wrapper = shallowMount(Alert, { propsData: props });
+          expect(wrapper.text()).toBe("test");
+          expect(wrapper.find("v-alert-stub").attributes("type")).toBe(t);
+        };
+
+        testType(MessageType.Info);
+        testType(MessageType.Success);
+        testType(MessageType.Warn);
+        testType(MessageType.Error);
+      });
+    });
+  });
+});
