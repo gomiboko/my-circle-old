94b8dd2e98b6f006c93390d5af7407bb00ba9c37
gomiboko <gomiboko1123@gmail.com>
2021-10-21 00:00:27 +0900

    [chore] linting

--- front-end/tests/unit/SmallLink.spec.ts
+++ front-end/tests/unit/SmallLink.spec.ts
@@ -2,65 +2,67 @@ import { shallowMount, createLocalVue } from "@vue/test-utils";
 import VueRouter from "vue-router";
 import SmallLink from "@/components/SmallLink.vue";
 
-const localVue = createLocalVue()
-localVue.use(VueRouter)
+const localVue = createLocalVue();
+localVue.use(VueRouter);
 
 describe("SmallLink.vue", () => {
   describe("textプロパティ", () => {
     const props = {
       to: "/",
       text: "",
-    }
+    };
 
     describe("空の場合", () => {
       test("何も表示されないこと", () => {
         props.text = "";
         const wrapper = shallowMount(SmallLink, {
           localVue,
-          propsData: props
-        })
-        expect(wrapper.text()).toBe("")
-      })
-    })
+          propsData: props,
+        });
+        expect(wrapper.text()).toBe("");
+      });
+    });
 
     describe("空でない場合", () => {
       test("textプロパティの値が表示されること", () => {
-        props.text = "テストテキスト"
+        props.text = "テストテキスト";
         const wrapper = shallowMount(SmallLink, {
           localVue,
-          propsData: props
-        })
-        expect(wrapper.text()).toBe("テストテキスト")
-      })
-    })
-  })
+          propsData: props,
+        });
+        expect(wrapper.text()).toBe("テストテキスト");
+      });
+    });
+  });
 
   describe("toプロパティ", () => {
     const props = {
       to: "",
-      text: "テストリンク"
-    }
+      text: "テストリンク",
+    };
 
     describe("空の場合", () => {
       test("リンク先に何も設定されていないこと", () => {
         props.to = "";
         const wrapper = shallowMount(SmallLink, {
           localVue,
-          propsData: props
-        })
-        expect(wrapper.find("router-link-stub").attributes("to")).toBe("")
-      })
-    })
+          propsData: props,
+        });
+        expect(wrapper.find("router-link-stub").attributes("to")).toBe("");
+      });
+    });
 
     describe("空でない場合", () => {
       test("リンク先がtoプロパティの値になっていること", () => {
         props.to = "/test/path";
         const wrapper = shallowMount(SmallLink, {
           localVue,
-          propsData: props
-        })
-        expect(wrapper.find("router-link-stub").attributes("to")).toBe("/test/path")
-      })
-    })
-  })
+          propsData: props,
+        });
+        expect(wrapper.find("router-link-stub").attributes("to")).toBe(
+          "/test/path"
+        );
+      });
+    });
+  });
 });
