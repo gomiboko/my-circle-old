03d017ada356381a62b6a3bc86da0c10230a2fa3
gomiboko <gomiboko1123@gmail.com>
2022-01-04 04:41:05 +0900

    [update] Github Actionsで実行時にエラーが出ていたので比較演算子を修正

--- back-end/execute_test.sh
+++ back-end/execute_test.sh
@@ -7,6 +7,6 @@ docker-compose -f $FILE_NAME up -d
 docker-compose -f $FILE_NAME exec -T test-back sh -c "go test -cover ./..."
 
 # ローカルでの実行時は、最後にコンテナを破棄する
-if [ "$1" == "local" ]; then
+if [ "$1" = "local" ]; then
   docker-compose -f $FILE_NAME down
 fi
