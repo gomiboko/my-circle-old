3f4a2c891492a53a9c0321f16103cc4c4ca909d1
gomiboko <gomiboko1123@gmail.com>
2022-04-16 22:47:13 +0900

    [clean] typo修正

--- front-end/src/views/Join.vue
+++ front-end/src/views/Join.vue
@@ -148,7 +148,7 @@ export default class Join extends Vue {
         }
       );
 
-      // トップページに繊維
+      // トップページに遷移
       this.$router.push("/");
     } finally {
       this.loading = false;
