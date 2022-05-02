339af2b1edb27f374a0e573b0b76e0203c5f33e1
gomiboko <gomiboko1123@gmail.com>
2022-05-02 16:47:24 +0900

    [add] アプリケーションバーにアカウントアイコンを追加

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -23,10 +23,23 @@
 
       <v-spacer></v-spacer>
 
-      <v-btn href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank" text>
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+      <!-- FIXME: ログイン中のみ表示させる -->
+      <v-menu offset-y nudge-bottom="5">
+        <template v-slot:activator="{ on }">
+          <v-avatar color="white" size="36" v-on="on" style="cursor: pointer" tabindex="-1">
+            <!-- TODO: プロフィール画像が設定済みかどうかで分岐させる -->
+            <v-icon light>mdi-account</v-icon>
+          </v-avatar>
+        </template>
+        <v-list dense>
+          <template v-for="(menu, index) in accountMenus">
+            <v-list-item v-if="menu.NAME" @click="onMenuClick(menu.ID)" :key="index">
+              <v-list-item-title>{{ menu.NAME }}</v-list-item-title>
+            </v-list-item>
+            <v-divider v-else :key="index"></v-divider>
+          </template>
+        </v-list>
+      </v-menu>
     </v-app-bar>
 
     <v-main>
@@ -48,10 +61,40 @@
 import { Vue, Component } from "vue-property-decorator";
 import AppMessage from "@/components/AppMessage.vue";
 
+const ACCOUNT_MENU_ITEMS = Object.freeze({
+  DIVIDER: Object.freeze({ ID: 0, NAME: "" }),
+  PROFILE: Object.freeze({ ID: 1, NAME: "プロフィール" }),
+  CONFIG: Object.freeze({ ID: 2, NAME: "設定" }),
+  LOGOUT: Object.freeze({ ID: 3, NAME: "ログアウト" }),
+});
+
 @Component({
   components: {
     AppMessage,
   },
 })
-export default class App extends Vue {}
+export default class App extends Vue {
+  get accountMenus(): Readonly<{ ID: number; NAME: string }>[] {
+    return [
+      ACCOUNT_MENU_ITEMS.PROFILE,
+      ACCOUNT_MENU_ITEMS.CONFIG,
+      ACCOUNT_MENU_ITEMS.DIVIDER,
+      ACCOUNT_MENU_ITEMS.LOGOUT,
+    ];
+  }
+
+  private onMenuClick(id: number) {
+    switch (id) {
+      case ACCOUNT_MENU_ITEMS.PROFILE.ID:
+        // TODO: プロフィール表示
+        break;
+      case ACCOUNT_MENU_ITEMS.CONFIG.ID:
+        // TODO: 設定表示
+        break;
+      case ACCOUNT_MENU_ITEMS.LOGOUT.ID:
+        // TODO: ログアウト処理
+        break;
+    }
+  }
+}
 </script>
