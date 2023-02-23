99bc8e4899377e4311c2b5b1bc3957e47fe0c168
gomiboko <gomiboko1123@gmail.com>
2023-02-23 16:45:06 +0900

    [update] バックエンド単体テストで、実行後必ずコンテナを破棄するように修正

--- back-end/execute_test.sh
+++ back-end/execute_test.sh
@@ -6,7 +6,4 @@ cd `dirname $0`
 docker-compose -f $FILE_NAME up -d
 docker-compose -f $FILE_NAME exec -T test-back sh -c "go test -cover ./..."
 
-# ローカルでの実行時は、最後にコンテナを破棄する
-if [ "$1" = "local" ]; then
-  docker-compose -f $FILE_NAME down
-fi
+docker-compose -f $FILE_NAME down
